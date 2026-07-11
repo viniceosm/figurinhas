54fef7dd28d7fd96332e8d2f0eb871f7f1572b1f
Vinícius Miiller Rebello <miiller.vini@gmail.com>
2026-07-11 03:38:07 +0000

    fix: corrige importmap

--- src/main.js
+++ src/main.js
@@ -1,15 +1,42 @@
-import { createApp, h } from "vue";
+import { createApp } from "vue";
+
+import { router } from "./router.js";
+
+import { IndexedDbAlbumRepository }
+    from "./repositories/IndexedDbAlbumRepository.js";
+
+import { AlbumService }
+    from "./services/AlbumService.js";
+
+const repository = new IndexedDbAlbumRepository();
+const albumService = new AlbumService(repository);
 
 const App = {
-  render() {
-    return h(
-      "div",
-      {
-        style: "padding:40px;color:white;font-size:32px"
-      },
-      "FUNCIONOU"
-    );
-  }
+    template: `
+        <div class="app">
+            <header class="app-header">
+                <router-link to="/" class="brand">
+                    🧩 Figuritas Manager
+                </router-link>
+            </header>
+
+            <main class="app-content">
+                <router-view />
+            </main>
+        </div>
+    `
 };
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+app.provide("albumService", albumService);
+
+app.use(router);
+console.log(router.getRoutes());
+
+await router.isReady();
+
+console.log("currentRoute", router.currentRoute.value);
+console.log("matched", router.currentRoute.value.matched);
+
+app.mount("#app");
