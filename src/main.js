1b3d7d2c1420b85500b28c79c9f5255f975a9528
Vinícius Miiller Rebello <miiller.vini@gmail.com>
2026-07-11 03:34:54 +0000

    chore: debug main

--- src/main.js
+++ src/main.js
@@ -1,42 +1,15 @@
-import { createApp } from "vue";
-
-import { router } from "./router.js";
-
-import { IndexedDbAlbumRepository }
-    from "./repositories/IndexedDbAlbumRepository.js";
-
-import { AlbumService }
-    from "./services/AlbumService.js";
-
-const repository = new IndexedDbAlbumRepository();
-const albumService = new AlbumService(repository);
+import { createApp, h } from "vue";
 
 const App = {
-    template: `
-        <div class="app">
-            <header class="app-header">
-                <router-link to="/" class="brand">
-                    🧩 Figuritas Manager
-                </router-link>
-            </header>
-
-            <main class="app-content">
-                <router-view />
-            </main>
-        </div>
-    `
+  render() {
+    return h(
+      "div",
+      {
+        style: "padding:40px;color:white;font-size:32px"
+      },
+      "FUNCIONOU"
+    );
+  }
 };
 
-const app = createApp(App);
-
-app.provide("albumService", albumService);
-
-app.use(router);
-console.log(router.getRoutes());
-
-await router.isReady();
-
-console.log("currentRoute", router.currentRoute.value);
-console.log("matched", router.currentRoute.value.matched);
-
-app.mount("#app");
+createApp(App).mount("#app");
